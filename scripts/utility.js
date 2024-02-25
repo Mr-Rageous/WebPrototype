5a691914a8e07ca1cafa075965f5f1a0e59a3b91
Soulin <the.second.effect@gmail.com>
2024-02-24 19:59:43 -0800

    refactored checkObjforProp function - 'utility.js'

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,13 +1,11 @@
 function checkObjectForProperty(property, object) {
-    if (!object[property]) {
-        for (var arrayProperty in object) {
-            if (Array.isArray(object[arrayProperty])) {
-                object[arrayProperty].forEach(element => {
-                    checkObjectForProperty(property, element);
-                });
-            }
+    if (object[property]) { return property; }
+    
+    for (var arrayProperty in object) {
+        if (Array.isArray(object[arrayProperty])) {
+            object[arrayProperty].forEach(element => {
+                checkObjectForProperty(property, element);
+            });
         }
-    }else{
-        return property;
     }
 }
