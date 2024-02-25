ff79b59289743cb52957381980731eb45491a3ba
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-02-24 23:08:11 -0500

    Fix bug in check for prop fn in utility.js

--- scripts/utility.js
+++ scripts/utility.js
@@ -2,8 +2,8 @@ function checkObjectForProperty(property, object) {
     if (object[property]) { return property; }
     
     object.values().forEach(property => {
-        if (Array.isArray(object[arrayProperty])) {
-            object[arrayProperty].forEach(element => {
+        if (Array.isArray(object[property])) {
+            object[property].forEach(element => {
                 checkObjectForProperty(property, element);
             });
         }
