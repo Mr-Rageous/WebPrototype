f26dc3768ab0b2218c7c96e94f854026507575a2
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-02-24 23:36:18 -0500

    Initialize eslint and prettier for codebase

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended", "prettier"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        'require-jsdoc' : 0,
+        'semi': [2, 'always']
+    }
+};
