a2dd08c086832eb87c82977f7ce317064bb1b75b
Soulin <the.second.effect@gmail.com>
2024-02-25 03:04:49 -0800

    moved items to items.js

--- scripts/item.js
+++ scripts/item.js
@@ -34,43 +34,5 @@ export class Item {
     }
 }
 
-const metal_short_blade = new Item(
-    'Metal Blade',
-    'A Metal Blade',
-    [ 'metal', 'blade' ],
-    [
-        { types: [{ type: 'hilt' }], quantity: 1, item: null }
-    ]
-);
-
-const metal_hilt = new Item(
-    'Metal Hilt',
-    'A Metal Hilt',
-    [ 'metal', 'hilt' ],
-    [
-        { types: [{ type: 'blade' }], quantity: 1, item: null },
-        { types: [{ type: 'handle' }], quantity: 1, item: null }
-    ]
-);
-
-const wooden_handle = new Item(
-    'Wooden Handle',
-    'A Wooden Handle',
-    [ 'wooden', 'handle' ],
-    [
-        { types: [{ type: 'hilt' }], quantity: 1, item: null },
-        { types: [{ type: 'pommel' }], quantity: 1, item: null }
-    ]
-);
-
-const metal_pommel = new Item(
-    'Metal Pommel',
-    'A Metal Pommel',
-    [ 'metal', 'pommel' ],
-    [
-        { types: [{ type: 'handle' }], quantity: 1, item: null }
-    ]
-);
-
 // working on sockets idea, probably gunna stick but unsure of limitations with it.
 // This turns the problem into the 'strongly connected components problem', therefore solution is the infamous Dijkstra's Algorithm.
--- scripts/items.js
+++ scripts/items.js
@@ -1,3 +1,39 @@
 import { Item } from './item.js';
 
-export const wooden_handle = new Item('Wooden Handle', 'A wooden handle', 'handle');
+const metal_short_blade = new Item(
+    'Metal Blade',
+    'A Metal Blade',
+    [ 'metal', 'blade' ],
+    [
+        { types: [{ type: 'hilt' }], quantity: 1, item: null }
+    ]
+);
+
+const metal_hilt = new Item(
+    'Metal Hilt',
+    'A Metal Hilt',
+    [ 'metal', 'hilt' ],
+    [
+        { types: [{ type: 'blade' }], quantity: 1, item: null },
+        { types: [{ type: 'handle' }], quantity: 1, item: null }
+    ]
+);
+
+const wooden_handle = new Item(
+    'Wooden Handle',
+    'A Wooden Handle',
+    [ 'wooden', 'handle' ],
+    [
+        { types: [{ type: 'hilt' }], quantity: 1, item: null },
+        { types: [{ type: 'pommel' }], quantity: 1, item: null }
+    ]
+);
+
+const metal_pommel = new Item(
+    'Metal Pommel',
+    'A Metal Pommel',
+    [ 'metal', 'pommel' ],
+    [
+        { types: [{ type: 'handle' }], quantity: 1, item: null }
+    ]
+);
